ce6602b1b2ac42850bff72c6bbc8b066b7b50683
Ivan <ipilat@vub.hr>
2022-06-02 18:58:18 +0200

    feat: get/set in class Osoba

--- src/Osoba.java
+++ src/Osoba.java
@@ -1,8 +1,53 @@
+import java.util.Date;
+
 public class Osoba {
     private String ime;
     private String prezime;
     private Boolean spol;
-    private String datumRodenja;
-    private String korisnikId;
-    private String kontaktBroj;
+    private Date datumRodenja;
+    private int korisnikId;
+    private int kontaktBroj;
+
+
+    public String getIme(){
+        return this.ime;
+    }
+    public void setIme(String ime){
+        this.ime=ime;
+    }
+
+    public String getPrezime(){
+        return this.prezime;
+    }
+    public void setPrezime(String prezime){
+        this.prezime=prezime;
+    }
+
+    public Boolean getSpol() {
+        return this.spol;
+    }
+    public void setSpol(Boolean spol){
+        this.spol=spol;
+    }
+
+    public Date getDatumRodenja(){
+        return this.datumRodenja;
+    }
+    public void setDatumRodenja(Date datumRodenja){
+        this.datumRodenja=datumRodenja;
+    }
+
+    public int getKorisnikId(){
+        return this.korisnikId;
+    }
+    public void setKorisnikId(int korisnikId){
+        this.korisnikId=korisnikId;
+    }
+
+    public int getKontaktBroj(){
+        return this.kontaktBroj;
+    }
+    public void setKontaktBroj(int kontaktBroj){
+        this.kontaktBroj=kontaktBroj;
+    }
 }
